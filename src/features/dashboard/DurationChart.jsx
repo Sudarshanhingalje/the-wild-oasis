9956900a4b136c2ed33d3e08bd6e0898a7cf41fc
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-02 09:25:27 +0530

    charts data apply

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,239 +1,100 @@
-import { Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { ResponsivePie } from "@nivo/pie";
 import styled from "styled-components";
 import { useDarkMode } from "../../context/DarkModeContext";
 import Heading from "../../ui/Heading";
 
 const ChartBox = styled.div`
-  /* Box */
   background-color: var(--color-grey-0);
   border: 1px solid var(--color-grey-100);
   border-radius: var(--border-radius-md);
-
-  padding: 2.4rem 3.2rem;
+  padding: 1rem 3.2rem;
   grid-column: 3 / span 2;
 
   & > *:first-child {
     margin-bottom: 1.6rem;
   }
-
-  & .recharts-pie-label-text {
-    font-weight: 600;
-  }
 `;
 
 const startDataLight = [
-  {
-    duration: "1 night",
-    value: 0,
-    color: "#ef4444",
-  },
-  {
-    duration: "2 nights",
-    value: 0,
-    color: "#f97316",
-  },
-  {
-    duration: "3 nights",
-    value: 0,
-    color: "#eab308",
-  },
-  {
-    duration: "4-5 nights",
-    value: 0,
-    color: "#84cc16",
-  },
-  {
-    duration: "6-7 nights",
-    value: 0,
-    color: "#22c55e",
-  },
-  {
-    duration: "8-14 nights",
-    value: 0,
-    color: "#14b8a6",
-  },
-  {
-    duration: "15-21 nights",
-    value: 0,
-    color: "#3b82f6",
-  },
-  {
-    duration: "21+ nights",
-    value: 0,
-    color: "#a855f7",
-  },
+  { id: "1 night", label: "1 night", value: 0, color: "#ef4444" },
+  { id: "2 nights", label: "2 nights", value: 0, color: "#f97316" },
+  { id: "3 nights", label: "3 nights", value: 0, color: "#eab308" },
+  { id: "4-5 nights", label: "4-5 nights", value: 0, color: "#84cc16" },
+  { id: "6-7 nights", label: "6-7 nights", value: 0, color: "#22c55e" },
+  { id: "8-14 nights", label: "8-14 nights", value: 0, color: "#14b8a6" },
+  { id: "15-21 nights", label: "15-21 nights", value: 0, color: "#3b82f6" },
+  { id: "21+ nights", label: "21+ nights", value: 0, color: "#a855f7" },
 ];
 
 const startDataDark = [
-  {
-    duration: "1 night",
-    value: 0,
-    color: "#b91c1c",
-  },
-  {
-    duration: "2 nights",
-    value: 0,
-    color: "#c2410c",
-  },
-  {
-    duration: "3 nights",
-    value: 0,
-    color: "#a16207",
-  },
-  {
-    duration: "4-5 nights",
-    value: 0,
-    color: "#4d7c0f",
-  },
-  {
-    duration: "6-7 nights",
-    value: 0,
-    color: "#15803d",
-  },
-  {
-    duration: "8-14 nights",
-    value: 0,
-    color: "#0f766e",
-  },
-  {
-    duration: "15-21 nights",
-    value: 0,
-    color: "#1d4ed8",
-  },
-  {
-    duration: "21+ nights",
-    value: 0,
-    color: "#7e22ce",
-  },
+  { id: "1 night", label: "1 night", value: 0, color: "#b91c1c" },
+  { id: "2 nights", label: "2 nights", value: 0, color: "#c2410c" },
+  { id: "3 nights", label: "3 nights", value: 0, color: "#a16207" },
+  { id: "4-5 nights", label: "4-5 nights", value: 0, color: "#4d7c0f" },
+  { id: "6-7 nights", label: "6-7 nights", value: 0, color: "#15803d" },
+  { id: "8-14 nights", label: "8-14 nights", value: 0, color: "#0f766e" },
+  { id: "15-21 nights", label: "15-21 nights", value: 0, color: "#1d4ed8" },
+  { id: "21+ nights", label: "21+ nights", value: 0, color: "#7e22ce" },
 ];
 
-function prepareData(startData, stays) {
-  // A bit ugly code, but sometimes this is what it takes when working with real data 😅
+function prepareData(template, stays) {
+  const result = [...template];
 
-  function incArrayValue(arr, field) {
-    return arr.map((obj) =>
-      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
-    );
-  }
+  stays.forEach(({ numNights }) => {
+    const match = (condition) => result.find((d) => d.id === condition);
 
-  const data = stays
-    .reduce((arr, cur) => {
-      const num = cur.numNights;
-      if (num === 1) return incArrayValue(arr, "1 night");
-      if (num === 2) return incArrayValue(arr, "2 nights");
-      if (num === 3) return incArrayValue(arr, "3 nights");
-      if ([4, 5].includes(num)) return incArrayValue(arr, "4-5 nights");
-      if ([6, 7].includes(num)) return incArrayValue(arr, "6-7 nights");
-      if (num >= 8 && num <= 14) return incArrayValue(arr, "8-14 nights");
-      if (num >= 15 && num <= 21) return incArrayValue(arr, "15-21 nights");
-      if (num >= 21) return incArrayValue(arr, "21+ nights");
-      return arr;
-    }, startData)
-    .filter((obj) => obj.value > 0);
+    if (numNights === 1) match("1 night").value += 1;
+    else if (numNights === 2) match("2 nights").value += 1;
+    else if (numNights === 3) match("3 nights").value += 1;
+    else if ([4, 5].includes(numNights)) match("4-5 nights").value += 1;
+    else if ([6, 7].includes(numNights)) match("6-7 nights").value += 1;
+    else if (numNights >= 8 && numNights <= 14) match("8-14 nights").value += 1;
+    else if (numNights >= 15 && numNights <= 21)
+      match("15-21 nights").value += 1;
+    else if (numNights > 21) match("21+ nights").value += 1;
+  });
 
-  return data;
+  return result.filter((item) => item.value > 0);
 }
+
 function DurationChart({ confirmedStays }) {
   const { isDarkMode } = useDarkMode();
   const startData = isDarkMode ? startDataDark : startDataLight;
-
   const data = prepareData(startData, confirmedStays);
 
   return (
     <ChartBox>
       <Heading as="h2">The average duration of bookings</Heading>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            // activeShape={renderActiveShape}
-            data={data}
-            cx="50%"
-            cy="50%"
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          />
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
+      <div style={{ height: "300px" }}>
+        <ResponsivePie
+          data={data}
+          margin={{ top: 20, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.6}
+          cornerRadius={2}
+          activeOuterRadiusOffset={8}
+          colors={({ data }) => data.color}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor={isDarkMode ? "#e5e7eb" : "#333"} // Tailwind gray-200
+          arcLabelsTextColor={{
+            from: "color",
+            modifiers: isDarkMode ? [["brighter", 2]] : [["darker", 2]],
+          }}
+          legends={[
+            {
+              anchor: "bottom",
+              direction: "row",
+              translateY: 56,
+              itemWidth: 100,
+              itemHeight: 18,
+              symbolShape: "circle",
+            },
+          ]}
+        />
+      </div>
     </ChartBox>
   );
 }
 
 export default DurationChart;
-
-// const data = [
-//   { name: 'Group A', value: 400 },
-//   { name: 'Group B', value: 300 },
-//   { name: 'Group C', value: 300 },
-//   { name: 'Group D', value: 200 },
-// ];
-
-// const renderActiveShape = (props) => {
-//   const RADIAN = Math.PI / 180;
-//   const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-//   const sin = Math.sin(-RADIAN * midAngle);
-//   const cos = Math.cos(-RADIAN * midAngle);
-//   const sx = cx + (outerRadius + 10) * cos;
-//   const sy = cy + (outerRadius + 10) * sin;
-//   const mx = cx + (outerRadius + 30) * cos;
-//   const my = cy + (outerRadius + 30) * sin;
-//   const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-//   const ey = my;
-//   const textAnchor = cos >= 0 ? 'start' : 'end';
-
-//   return (
-//     <g>
-//       <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-//         {payload.name}
-//       </text>
-//       <Sector
-//         cx={cx}
-//         cy={cy}
-//         innerRadius={innerRadius}
-//         outerRadius={outerRadius}
-//         startAngle={startAngle}
-//         endAngle={endAngle}
-//         fill={fill}
-//       />
-//       <Sector
-//         cx={cx}
-//         cy={cy}
-//         startAngle={startAngle}
-//         endAngle={endAngle}
-//         innerRadius={outerRadius + 6}
-//         outerRadius={outerRadius + 10}
-//         fill={fill}
-//       />
-//       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-//       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-//       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
-//       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-//         {`(Rate ${(percent * 100).toFixed(2)}%)`}
-//       </text>
-//     </g>
-//   );
-// };
-
-// export default class Example extends PureComponent {
-
-//   render() {
-//     return (
-//       <ResponsiveContainer width="100%" height="100%">
-//         <PieChart width={400} height={400}>
-//           <Pie
-//             activeShape={renderActiveShape}
-//             data={data}
-//             cx="50%"
-//             cy="50%"
-//             innerRadius={60}
-//             outerRadius={80}
-//             fill="#8884d8"
-//             dataKey="value"
-//           />
-//         </PieChart>
-//       </ResponsiveContainer>
-//     );
-//   }
-// }
+//nvo lib used ,aslo use rechart , d3 lib charts
